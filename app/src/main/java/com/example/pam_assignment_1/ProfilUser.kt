f8c6842861732e72b68e48576d5e19790d65201c
AliSulthon <muhammadsylthan16@gmail.com>
2025-09-25 13:29:25 +0700

    Struktur Profil User

---
+++ app/src/main/java/com/example/pam_assignment_1/ProfilUser.kt
@@ -0,0 +1,20 @@
+package com.example.pam_assignment_1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProfilUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profil_user)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
